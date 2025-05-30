0a6d9f2f3f3cc4574fa65cfcc51dbe7c8c446e62
Hailey <hailey@example.com>
2025-05-30 12:04:09 -0500

    more fps

--- WithCharMemory/hdl/tb.cpp
+++ WithCharMemory/hdl/tb.cpp
@@ -46,8 +46,8 @@ float g_fontsize = 12;
 
 struct VgaTv
 {
-	int height, width, counter, pixels_len;
-	Color *pixels;
+	int height, width;
+	RenderTexture2D texture;
 };
 
 /* clocks_w, h are total size of screen, not just visable areia */
@@ -56,34 +56,21 @@ VgaTv new_tv(int clocks_w, int clocks_h)
 	size_t sz = sizeof(Color) * (clocks_w * clocks_h);
 	VgaTv ret = {
 		.height = clocks_h,
-		.width = clocks_w,
-		.pixels_len = clocks_w * clocks_h,
-		.pixels = (Color *)malloc(sz)};
-	memset(ret.pixels, 0, sz);
+		.width = clocks_w};
+
+	ret.texture = LoadRenderTexture(clocks_w, clocks_h);
 	return ret;
 }
 
 void DrawTv(VgaTv t, int px, int py)
 {
 	DrawRectangle(px, py, t.width, t.height, rgb(190, 190, 190));
-
-	for (size_t y = 0; y < t.height; y++)
-		for (size_t x = 0; x < t.width; x++)
-		{
-			Color c = t.pixels[y * t.width + x];
-			if (ColorIsEqual(c, BLANK))
-				continue;
-
-			DrawPixel(px + x, py + y, c);
-		}
+	DrawTextureRec(t.texture.texture, (Rectangle){0, 0, (float)t.texture.texture.width, (float)-t.texture.texture.height}, (Vector2){px, py}, WHITE);
 }
 
 void CaptureTv(VgaTv *t, u_char clk, u_char r, u_char g, u_char b, int hc, int vc)
 {
-	// if (t->counter > t->pixels_len)
-	// 	t->counter = 0;
-	t->pixels[vc * t->width + hc] = (Color){r, g, b, 255};
-	// t->counter += 1;
+	DrawPixel(hc, vc, (Color){r, g, b, 255});
 }
 
 int main(int argc, char *argv[])
@@ -92,7 +79,7 @@ int main(int argc, char *argv[])
 
 #ifdef GRAPHICAL_TB
 	g_fontsize = 12;
-	InitWindow(1000, 800, "VTB");
+	InitWindow(900, 700, "VTB");
 	SetTargetFPS(60);
 
 	VgaTv tv = new_tv(800, 521);
@@ -104,29 +91,28 @@ int main(int argc, char *argv[])
 
 		if (IsKeyPressed(KEY_S) || IsKeyDown(KEY_SPACE) || IsKeyDown(KEY_F) || go)
 		{
+			BeginTextureMode(tv.texture);
 			CaptureTv(&tv, dut->clk, dut->red * 4, dut->green * 4, dut->blue * 4, dut->dhc, dut->dvc);
+			EndTextureMode();
+
 			dut->eval();
 			dut->clk = !dut->clk;
 		}
 
-		if (IsKeyPressed(KEY_W))
+		if (IsKeyDown(KEY_W))
 		{
-			for (size_t i = 0; i < (800 * 512) * 2; i++)
+			BeginTextureMode(tv.texture);
+
+			float doneat = GetTime() + 1.0f / 60.0f;
+			for (size_t i = 0;i < (800 * 2) * 521 ; i ++)
 			{
 				CaptureTv(&tv, dut->clk, dut->red * 4, dut->green * 4, dut->blue * 4, dut->dhc, dut->dvc);
 				dut->eval();
 				dut->clk = !dut->clk;
+				if(GetTime() > doneat) break;
 			}
-		}
-
-		if (IsKeyPressed(KEY_F))
-		{
-			SetTargetFPS(1000);
-		}
-
-		if (IsKeyReleased(KEY_F))
-		{
-			SetTargetFPS(60);
+			
+			EndTextureMode();
 		}
 
 		if (IsKeyPressed(KEY_G))
